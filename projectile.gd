faafff56ddc98b425c72f5e5aac264d3f72c4e49
Comrade SparklePony <dmitri.s.volkov@gmail.com>
2018-04-18 09:11:13 -0400

    upload files with fixed projectile
    I haven't figured out how to properly use git yet.

--- projectile.gd
+++ projectile.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
 export (int) var SPEED
-var direction
+var move_vector
+var detect_pos
 
 func _ready():
-	direction = get_angle_to(get_global_mouse_position())
+	detect_pos = true
 
 func _process(delta):
-	position += Vector2(SPEED, 0)+direction * delta
+	if detect_pos:
+		move_vector = get_global_mouse_position() - position
+		move_vector = move_vector.normalized()
+	position += move_vector * delta * SPEED
+	detect_pos = false
