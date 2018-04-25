9d1d9b244fd52428a73819b322ec789add63d15f
Comrade SparklePony <comradesparklepony@gmail.com>
2018-04-25 18:50:25 -0400

    finished maze code
    finished maze code, added player to maze, changed world wall so it was in group "wall"

--- maze.gd
+++ maze.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export (PackedScene) var wall
+export (PackedScene) var player
 export (int) var MAZESIZE
 export (int) var TUNNELS
 
@@ -25,7 +26,6 @@ func _ready():
 	for y in range(MAZESIZE):
 		for x in range(MAZESIZE):
 			wall_array[y].append(0)
-	print(wall_array)
 	
 	
 	#get random starting point and direction
@@ -41,16 +41,39 @@ func _ready():
 			direction = vert_directions[randi() % 2]
 		
 		length = (randi() % (MAZESIZE - 1)) + 1
-		print(length)
 		
 		#repeat for length and as long as not colliding with wall
 		while length > 0 and not (maze_point.x == 0 or maze_point.y == 0 or maze_point.x == MAZESIZE - 1 or maze_point.y == MAZESIZE - 1):
 			wall_array[maze_point.y][maze_point.x] = 1
 			maze_point += direction
-			print(maze_point)
-		
+			length -= 1
+		if (maze_point.x == 0 or maze_point.y == 0 or maze_point.x == MAZESIZE - 1 or maze_point.y == MAZESIZE - 1):
+			maze_point = Vector2((randi() % MAZESIZE), (randi() % MAZESIZE))
 		tunnels -= 1
 	
-	for i in range(MAZESIZE):
-		print(wall_array[i])
-
+	# PLACE WALLS
+	
+	maze_point = Vector2(0,0)
+	var new_wall
+	
+	for y in wall_array:
+		for x in y:
+			if x == 0:
+				new_wall = wall.instance()
+				new_wall.position = maze_point
+				add_child(new_wall)
+			maze_point.x += 100
+		maze_point.y += 100
+		maze_point.x = 0
+	
+	#spawn player at an open area
+	var spawn_player
+	var player_spawned = false
+	for y in range(MAZESIZE):
+		for x in range(MAZESIZE):
+			if wall_array[y][x] == 1 and not player_spawned:
+				spawn_player = player.instance()
+				spawn_player.position = Vector2(x, y) * 100
+				add_child(spawn_player)
+				player_spawned = true
+				
